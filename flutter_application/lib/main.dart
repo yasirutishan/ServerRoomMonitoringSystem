da99969aaffecf3e341d783508d3dd615e09a0ed
yasirutishan <yasirutishan@hotmail.com>
2023-03-02 22:53:06 +0530

    adding graphs for testing

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,125 +1,144 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  return runApp(_ChartApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _ChartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: _MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MyHomePage extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  _MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyHomePageState extends State<_MyHomePage> {
+  List<_SalesData> data = [
+    _SalesData('Jan', 35),
+    _SalesData('Feb', 28),
+    _SalesData('Mar', 34),
+    _SalesData('Apr', 32),
+    _SalesData('May', 40)
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final _dbRef = FirebaseDatabase.instance.ref('sensor_1_data');
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Syncfusion Flutter chart'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(children: [
+        StreamBuilder(
+          stream: _dbRef.onValue,
+          builder: (context, snapshot) {
+            List<String> messageList = [];
+            if (snapshot.hasData &&
+                snapshot.data != null &&
+                (snapshot.data!).snapshot.value != null) {
+              final myMessages = Map<dynamic, dynamic>.from((snapshot.data!)
+                  .snapshot
+                  .value as Map<dynamic, dynamic>); //typecasting
+              myMessages.forEach((key, value) {
+                final currentMessage = Map<String, dynamic>.from(value);
+                // messageList.add(Message(
+                //     author: currentMessage['Author'],
+                //     authorId: currentMessage['Author_ID'],
+                //     text: currentMessage['Text'],
+                //     time: currentMessage['Time'],));
+              }); //created a class called message and added all messages in a List of class message
+              return ListView.builder(
+                reverse: true,
+                itemCount: messageList.length,
+                itemBuilder: (context, index) {
+                  return const Text("data");
+                  //
+                  // ChattingDesign(
+                  //   message: messageList[index],
+                  //   dbpathToMsgChnl:
+                  //       'TextChannels/${widget.channels['ChannelName']}/Messages',
+                  //   showName: shouldShowName(
+                  //     index,
+                  //     messageList.length - 1,
+                  //     messageList,
+                  //   ),
+                  // );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text(
+                  'Say Hi...',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 21,
+                      fontWeight: FontWeight.w400),
+                ),
+              );
+            }
+          },
+        )
+      ]
+          // //Initialize the chart widget
+          // SfCartesianChart(
+          //   primaryXAxis: CategoryAxis(),
+          //   // Chart title
+          //   title: ChartTitle(text: 'Half yearly sales analysis'),
+          //   // Enable legend
+          //   legend: Legend(isVisible: true),
+          //   // Enable tooltip
+          //   tooltipBehavior: TooltipBehavior(enable: true),
+          //   series: <ChartSeries<_SalesData, String>>[
+          //     LineSeries<_SalesData, String>(
+          //         dataSource: [
+          //           _SalesData('Jan', 11),
+          //           _SalesData('Feb', 28),
+          //           _SalesData('Mar', 22),
+          //           _SalesData('Apr', 32),
+          //           _SalesData('May', 33),
+          //           _SalesData('jun', 50),
+          //           _SalesData('jul', 55),
+          //           _SalesData('aug', 34),
+          //           _SalesData('sep', 7),
+          //           _SalesData('oct', 40),
+          //           _SalesData('now', 66),
+          //           _SalesData('dec', 9),
+          //         ],
+          //         xValueMapper: (_SalesData sales, _) => sales.year,
+          //         yValueMapper: (_SalesData sales, _) => sales.sales,
+          //         name: 'Sales',
+          //         // Enable data label
+          //         dataLabelSettings: const DataLabelSettings(isVisible: true))
+          //   ],
+          // ),
+
+          ),
     );
   }
+}
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
 }
